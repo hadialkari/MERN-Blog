57fb7d4ca81ce42ecdd1cfe4265a69699cfe0536
Hadi Alk <Hade.Kary24@gmail.com>
2024-04-06 22:43:21 +0300

    Complete create a post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -11,7 +11,7 @@ import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -208,7 +208,12 @@ export default function DashProfile() {
           placeholder='password'
           onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline> Update </Button>
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}> {loading ? 'Loading...' : 'Update'} </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>Create a Post</Button>
+          </Link>
+        )}
           
       </form>
       <div className='text-red-500 flex justify-between mt-5'>

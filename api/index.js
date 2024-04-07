400cee36f7e22a2c25ba65a4213581b99febd94a
Hadi Alk <Hade.Kary24@gmail.com>
2024-04-07 08:31:03 +0300

    Add create a post API route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js';
+
 
 
 
@@ -22,12 +24,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;

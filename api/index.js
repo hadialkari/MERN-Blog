a5f2c76d953dd387bb23305b71e8b5e7797cf855
Hadi Alk <Hade.Kary24@gmail.com>
2024-04-10 17:45:58 +0300

    Add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import commentRoutes from './routes/comment.route.js';
 import postRoutes from './routes/post.route.js';
 
 
@@ -32,6 +33,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 
 app.use((err, req, res, next) => {

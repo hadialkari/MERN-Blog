4a240a7e1115bd0fb3b2536c81991755e7c3a935
Hadi Alk <Hade.Kary24@gmail.com>
2024-04-10 22:35:14 +0300

    Add like functionality to the comment component

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,8 +8,9 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-router.get('/:userId', getUser);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
+
 
 
 export default router;
